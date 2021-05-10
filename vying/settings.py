68fdfc3d4e07aae5145dd54896a1d844b50a4842
jedgodsey <jed.godsey@gmail.com>
2021-05-10 14:06:23 -0700

    running at home and connected to db

--- vying/settings.py
+++ vying/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_5m*+kk10#luy8f^!e*xk(-yosir)ljf#du=oe&v5n&2jbn&3k'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
