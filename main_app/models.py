447ef571c4c365dae21243c0a20cda54f500c587
jedgodsey <jed.godsey@gmail.com>
2021-05-16 20:53:51 -0700

    finished models v1

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.translation import gettext
+import datetime
 
-# Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.name
+
+class Event(models.Model):
+    title = models.CharField(max_length = 150)
+    description = models.TextField(max_length = 2000)
+
+    def __str__(self):
+        return self.title
+
+class Performance(models.Model):
+    date = models.DateField(gettext("Date"), default=datetime.date.today)
+    event = models.ForeignKey(Event, on_delete = models.CASCADE)
+    competitor = models.ForeignKey(Profile, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.competitor
