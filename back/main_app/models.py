29a9a6054af45b6dfa2cfeb26832d6de2196b2d5
jedgodsey <jed.godsey@gmail.com>
2021-05-20 22:12:14 -0700

    filled out models

--- back/main_app/models.py
+++ back/main_app/models.py
@@ -4,12 +4,19 @@ import datetime
 
 class Profile(models.Model):
     name = models.CharField(max_length = 100)
+    birthdate = models.DateField()
+    sex = models.CharField(max_length = 6, choices = [('M', 'Male'), ('F', 'Female')], default = 'F')
+    email = models.EmailField(max_length = 50)
+    image = models.URLField()
     def __str__(self):
         return self.name
 
 class Event(models.Model):
     title = models.CharField(max_length = 150)
     description = models.TextField(max_length = 2000)
+    creator = models.ForeignKey(Profile, on_delete = models.CASCADE)
+    measurement = models.CharField(max_length=6, choices=[('Time', 'Time'),('Number', 'Number')])
+    performance_direction = models.CharField(max_length=4, choices=[('High', 'High'),('Low','Low')])
     def __str__(self):
         return self.title
 
@@ -17,5 +24,7 @@ class Performance(models.Model):
     date = models.DateField(gettext("Date"), default=datetime.date.today)
     event = models.ForeignKey(Event, on_delete = models.CASCADE)
     competitor = models.ForeignKey(Profile, on_delete = models.CASCADE)
+    time_performance = models.DurationField()
+    float_performance = models.DecimalField(max_digits=15, decimal_places=2)
     def __str__(self):
         return self.competitor
