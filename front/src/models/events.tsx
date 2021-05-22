9381cd5dc3de5958e2cad051035e209d0e355faa
jedgodsey <jed.godsey@gmail.com>
2021-05-21 22:47:14 -0700

    created event models in front

--- front/src/models/events.tsx
+++ front/src/models/events.tsx
@@ -1,12 +1,12 @@
-let url: string = 'http://localhost:8000/profiles/';
+let url: string = 'http://localhost:8000/events/';
 
-class ProfileModel {
+class EventModel {
     static all() {
         return fetch(url)
             .then(res => res.json())
             .catch(err => {
-                console.log('error fetching data in ProfileModel.all: ', err)
-                return {object: []};
+                console.log('error fetching data in EventModel.all: ', err)
+                return {object: []}
             })
     }
 
@@ -14,48 +14,48 @@ class ProfileModel {
         return fetch(`${url}/${id}/`)
             .then(res => res.json())
             .catch(err => {
-                console.log('error getting item in ProfileModel.getOne: ', err)
+                console.log('error getting item in EventModel.getOne: ', err)
                 return {profile: {}}
             })
     }
 
-    static create(newProfile: object) {
+    static create(newEvent: object) {
         return fetch(url, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(newProfile),
+            body: JSON.stringify(newEvent),
             credentials: 'include'
         })
-            .then(res => window.location.href = '/dashboard')
+            .then(res => window.location.href = '/dashboard') //return new object instead?
     }
 
-    static update(updatedProfile: any) {
-        return fetch(`${url}/${updatedProfile.id}/`, {
+    static update(updatedEvent: any) {
+        return fetch(`${url}/${updatedEvent.id}/`, {
             method: 'PUT',
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(updatedProfile),
+            body: JSON.stringify(updatedEvent),
             credentials: 'include'
         })
             .then(res => res.json())
             .catch(err => {
-                console.log('error updating profile in ProfileModel.update: ', err)
+                console.log('error updating item in EventModel.update: ', err)
                 return {message: 'error in update'}
             })
-            .then(res => window.location.href = '/dashboard')
+            .then(res => window.location.href = '/dashboard') //where to redirect to?
     }
 
     static delete(id: number) {
-        fetch(`${url}/${id}`, {
+        fetch(`${url}/${id}/`, {
             method: 'DELETE',
             credentials: 'include'
         })
-            .then(res => res)
+            .then(res => res) //is this correct?
             .catch(err => {
-                console.log('error deleting profile in ProfileModel.delete: ', err)
+                console.log('error deleting event in EventModel.delete: ', err)
                 return {message: 'error in delete'}
             })
             // .then(res => res.status === 200 ? window.location.href = '/': null)
     }
 }
 
-export default ProfileModel
+export default EventModel
