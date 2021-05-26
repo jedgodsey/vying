d7da5060be7a955ef002b93283c597ac16f9f6c4
jedgodsey <jed.godsey@gmail.com>
2021-05-25 22:28:12 -0700

    events displaying

--- front/src/pages/ListEvents.tsx
+++ front/src/pages/ListEvents.tsx
@@ -1,18 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import EventModel from '../models/event'
 
-const ListEvents = () => {
-    let [ events, setEvents ] = useState([])
+const ListEvents: React.FC = () => {
+    let [ events, setEvents ] = useState<any[]>([])
 
     const fill = async () => {
         await EventModel.all().then(data => {
-            console.log(data)
+            setEvents(data)
         })
     }
+    
+    useEffect(() => {      
+        fill();
+    }, [])
     return(
         <>
             <h1>These are all the events</h1>
-            {fill()}
+            <ul>
+            {events && events.map(item => {
+                return(
+                    <li>{item.title}  {item.description}</li>
+                )
+            })}
+            </ul>
         </>
     )
 }
